34b39b267c607ebf6677834c3988b2ad2ecf052d
MurphyBLG <erwin16@ya.ru>
2023-09-09 21:06:33 +0500

    initial commit

---
+++ Lab1P1/Lab1P1/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Lab1P1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.EditToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ProgrammAuthorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripSeparator();
+            this.AboutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.AuthorNameLabel = new System.Windows.Forms.Label();
+            this.AuthorNameTextBox = new System.Windows.Forms.TextBox();
+            this.SaveAuthorButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.справкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.EditToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.ExitToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(59, 26);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // EditToolStripMenuItem
+            // 
+            this.EditToolStripMenuItem.Name = "EditToolStripMenuItem";
+            this.EditToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.EditToolStripMenuItem.Text = "Редактирование";
+            this.EditToolStripMenuItem.Click += new System.EventHandler(this.EditToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(221, 6);
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ExitToolStripMenuItem.Text = "Выход";
+            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ProgrammAuthorToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem,
+            this.AboutToolStripMenuItem1});
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(81, 26);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // ProgrammAuthorToolStripMenuItem
+            // 
+            this.ProgrammAuthorToolStripMenuItem.Enabled = false;
+            this.ProgrammAuthorToolStripMenuItem.Name = "ProgrammAuthorToolStripMenuItem";
+            this.ProgrammAuthorToolStripMenuItem.Size = new System.Drawing.Size(221, 26);
+            this.ProgrammAuthorToolStripMenuItem.Text = "Автор программы";
+            this.ProgrammAuthorToolStripMenuItem.Click += new System.EventHandler(this.ProgrammAuthorToolStripMenuItem_Click);
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(218, 6);
+            // 
+            // AboutToolStripMenuItem1
+            // 
+            this.AboutToolStripMenuItem1.Name = "AboutToolStripMenuItem1";
+            this.AboutToolStripMenuItem1.Size = new System.Drawing.Size(221, 26);
+            this.AboutToolStripMenuItem1.Text = "О программе";
+            this.AboutToolStripMenuItem1.Click += new System.EventHandler(this.AboutToolStripMenuItem1_Click);
+            // 
+            // AuthorNameLabel
+            // 
+            this.AuthorNameLabel.AutoSize = true;
+            this.AuthorNameLabel.Location = new System.Drawing.Point(308, 147);
+            this.AuthorNameLabel.Name = "AuthorNameLabel";
+            this.AuthorNameLabel.Size = new System.Drawing.Size(126, 16);
+            this.AuthorNameLabel.TabIndex = 1;
+            this.AuthorNameLabel.Text = "Автор программы:";
+            // 
+            // AuthorNameTextBox
+            // 
+            this.AuthorNameTextBox.Location = new System.Drawing.Point(275, 186);
+            this.AuthorNameTextBox.Name = "AuthorNameTextBox";
+            this.AuthorNameTextBox.Size = new System.Drawing.Size(262, 22);
+            this.AuthorNameTextBox.TabIndex = 2;
+            // 
+            // SaveAuthorButton
+            // 
+            this.SaveAuthorButton.Location = new System.Drawing.Point(275, 234);
+            this.SaveAuthorButton.Name = "SaveAuthorButton";
+            this.SaveAuthorButton.Size = new System.Drawing.Size(262, 23);
+            this.SaveAuthorButton.TabIndex = 3;
+            this.SaveAuthorButton.Text = "Сохранить";
+            this.SaveAuthorButton.UseVisualStyleBackColor = true;
+            this.SaveAuthorButton.Click += new System.EventHandler(this.SaveAuthorButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SaveAuthorButton);
+            this.Controls.Add(this.AuthorNameTextBox);
+            this.Controls.Add(this.AuthorNameLabel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ProgrammAuthorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.Label AuthorNameLabel;
+        private System.Windows.Forms.TextBox AuthorNameTextBox;
+        private System.Windows.Forms.ToolStripMenuItem AboutToolStripMenuItem1;
+        private System.Windows.Forms.Button SaveAuthorButton;
+        private System.Windows.Forms.ToolStripMenuItem EditToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
+    }
+}
+
---
+++ Lab1P1/Lab1P1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1P1
+{
+    public partial class Form1 : Form
+    {
+        private string _authorName = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EditToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AuthorNameLabel.Visible = !AuthorNameLabel.Visible;
+            AuthorNameTextBox.Visible = !AuthorNameTextBox.Visible;
+            SaveAuthorButton.Visible = !SaveAuthorButton.Visible;
+        }
+
+        private void AboutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "Лабораторная работа №1" + Environment.NewLine + "Версия 1.0.0", "Сведения о программе");
+        }
+
+        private void SaveAuthorButton_Click(object sender, EventArgs e)
+        {
+            _authorName = AuthorNameTextBox.Text;
+            ChangeAboutAuthorVisibility();
+        }
+
+        private void ChangeAboutAuthorVisibility()
+        {
+            if (string.IsNullOrEmpty(_authorName))
+                ProgrammAuthorToolStripMenuItem.Enabled = false;
+            else
+                ProgrammAuthorToolStripMenuItem.Enabled = true;
+        }
+
+        private void ProgrammAuthorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "Имя автора: " + _authorName, "Сведения об авторе");
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
