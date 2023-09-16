05f436928e731186df1fec4f2028d9e78ce27364
MurphyBLG <vitaliy.ryavkin@gmail.com>
2023-09-16 09:59:08 +0500

    unnecessary usings removed and file scoped namespce added

--- Lab1P4/Lab1P4/CreateDotForm.cs
+++ Lab1P4/Lab1P4/CreateDotForm.cs
@@ -1,36 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿namespace Lab1P4;
 
-namespace Lab1P4
+public partial class CreateDotForm : Form
 {
-    public partial class CreateDotForm : Form
+    private readonly Bitmap _bitmap;
+    private readonly Color _color;
+    public CreateDotForm(Bitmap bitmap, Color color)
     {
-        private readonly Bitmap _bitmap;
-        private readonly Color _color;
-        public CreateDotForm(Bitmap bitmap, Color color)
-        {
-            InitializeComponent();
-            _bitmap = bitmap;
-            _color = color;
-        }
+        InitializeComponent();
+        _bitmap = bitmap;
+        _color = color;
+    }
 
-        private void CreateDotButton_Click(object sender, EventArgs e)
-        {
-            using var g = Graphics.FromImage(_bitmap);
-            var brush = new SolidBrush(_color);
-            var xCord = Convert.ToInt32(XCordTextBox.Text);
-            var yCord = Convert.ToInt32(YCordTextBox.Text);
-            var radius = 5;
-            g.FillEllipse(brush, xCord - radius, yCord-radius, 2*radius, 2 * radius);
-            g.Flush();
-            this.Close();
-        }
+    private void CreateDotButton_Click(object sender, EventArgs e)
+    {
+        using var g = Graphics.FromImage(_bitmap);
+        var brush = new SolidBrush(_color);
+        var xCord = Convert.ToInt32(XCordTextBox.Text);
+        var yCord = Convert.ToInt32(YCordTextBox.Text);
+        var radius = 5;
+        g.FillEllipse(brush, xCord - radius, yCord - radius, 2 * radius, 2 * radius);
+        g.Flush();
+        this.Close();
     }
 }
