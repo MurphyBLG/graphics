1b00ee4d328c74cb1221fc44c89669978ffda301
MurphyBLG <vitaliy.ryavkin@gmail.com>
2023-11-07 06:52:13 +0500

    mrak

---
+++ Lab3/Lab3/MainForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Lab3
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            splitContainer1 = new SplitContainer();
+            LineByLineFillingButton = new Button();
+            SeedFillingButton = new Button();
+            PickColorButton = new Button();
+            RefreshButton = new Button();
+            MainPictureBox = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)MainPictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(LineByLineFillingButton);
+            splitContainer1.Panel1.Controls.Add(SeedFillingButton);
+            splitContainer1.Panel1.Controls.Add(PickColorButton);
+            splitContainer1.Panel1.Controls.Add(RefreshButton);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(MainPictureBox);
+            splitContainer1.Size = new Size(1264, 681);
+            splitContainer1.SplitterDistance = 245;
+            splitContainer1.TabIndex = 0;
+            // 
+            // LineByLineFillingButton
+            // 
+            LineByLineFillingButton.Location = new Point(12, 199);
+            LineByLineFillingButton.Name = "LineByLineFillingButton";
+            LineByLineFillingButton.Size = new Size(221, 42);
+            LineByLineFillingButton.TabIndex = 2;
+            LineByLineFillingButton.Text = "Построчная заливка";
+            LineByLineFillingButton.UseVisualStyleBackColor = true;
+            LineByLineFillingButton.Click += LineByLineFillingButton_Click;
+            // 
+            // SeedFillingButton
+            // 
+            SeedFillingButton.Location = new Point(12, 144);
+            SeedFillingButton.Name = "SeedFillingButton";
+            SeedFillingButton.Size = new Size(221, 49);
+            SeedFillingButton.TabIndex = 1;
+            SeedFillingButton.Text = "Заливка с затравкой";
+            SeedFillingButton.UseVisualStyleBackColor = true;
+            SeedFillingButton.Click += SeedFillingButton_Click;
+            // 
+            // PickColorButton
+            // 
+            PickColorButton.Location = new Point(12, 74);
+            PickColorButton.Name = "PickColorButton";
+            PickColorButton.Size = new Size(221, 64);
+            PickColorButton.TabIndex = 1;
+            PickColorButton.Text = "Выбрать цвет";
+            PickColorButton.UseVisualStyleBackColor = true;
+            PickColorButton.Click += PickColorButton_Click;
+            // 
+            // RefreshButton
+            // 
+            RefreshButton.Location = new Point(12, 12);
+            RefreshButton.Name = "RefreshButton";
+            RefreshButton.Size = new Size(221, 56);
+            RefreshButton.TabIndex = 0;
+            RefreshButton.Text = "Перерисовать фигуру";
+            RefreshButton.UseVisualStyleBackColor = true;
+            RefreshButton.Click += RefreshButton_Click;
+            // 
+            // MainPictureBox
+            // 
+            MainPictureBox.Location = new Point(3, 3);
+            MainPictureBox.Name = "MainPictureBox";
+            MainPictureBox.Size = new Size(1008, 675);
+            MainPictureBox.TabIndex = 0;
+            MainPictureBox.TabStop = false;
+            MainPictureBox.MouseClick += MainPictureBox_MouseClick;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1264, 681);
+            Controls.Add(splitContainer1);
+            Name = "MainForm";
+            Text = "Form1";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)MainPictureBox).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private PictureBox MainPictureBox;
+        private Button RefreshButton;
+        private Button PickColorButton;
+        private Button SeedFillingButton;
+        private Button LineByLineFillingButton;
+    }
+}
---
+++ Lab3/Lab3/MainForm.cs
@@ -0,0 +1,192 @@
+﻿using System.Drawing;
+using static System.Windows.Forms.AxHost;
+
+namespace Lab3;
+
+public partial class MainForm : Form
+{
+    private Color _fillColor = ColorTranslator.FromHtml("#333333");
+    private readonly List<PointF> _originalPoints = new()
+    {
+        new PointF(300, 150),
+        new PointF(300, 270),
+        new PointF(420, 240),
+        new PointF(540, 270),
+        new PointF(540, 150),
+        new PointF(420, 180)
+    };
+
+    private readonly List<PointF> _originalMiddleLine = new()
+    {
+        new PointF(420, 240),
+        new PointF(420, 180)
+    };
+
+    private Point? _selectedPoint;
+
+    private readonly Bitmap _bitmap;
+
+    public MainForm()
+    {
+        InitializeComponent();
+        _bitmap = new Bitmap(MainPictureBox.Width, MainPictureBox.Height);
+        MainPictureBox.Image = (Image)_bitmap;
+        MainPictureBox.BackgroundImageLayout = ImageLayout.None;
+        _selectedPoint = null;
+        DrawFigure();
+    }
+
+    private void DrawFigure()
+    {
+        ClearImage();
+        using var g = Graphics.FromImage(_bitmap);
+        var pen = new Pen(Color.Black);
+
+        for (var i = 0; i < _originalPoints.Count; i++)
+            g.DrawLine(pen, _originalPoints[i], _originalPoints[(i + 1) % _originalPoints.Count]);
+
+        for (var i = 0; i < _originalMiddleLine.Count; i++)
+            g.DrawLine(pen, _originalMiddleLine[i], _originalMiddleLine[(i + 1) % _originalMiddleLine.Count]);
+
+        g.Flush();
+        MainPictureBox.Invalidate();
+    }
+
+    private void ClearImage()
+    {
+        var g = Graphics.FromImage(_bitmap);
+        g.Clear(Color.Empty);
+    }
+
+    private void RefreshButton_Click(object sender, EventArgs e)
+    {
+        DrawFigure();
+    }
+
+    private void PickColorButton_Click(object sender, EventArgs e)
+    {
+        var colorDialog = new ColorDialog();
+
+        if (colorDialog.ShowDialog() == DialogResult.OK)
+            _fillColor = colorDialog.Color;
+    }
+
+    private void MainPictureBox_MouseClick(object sender, MouseEventArgs e)
+    {
+        _selectedPoint = new Point(e.X, e.Y);
+    }
+
+    public bool IsPointInPolygon(List<PointF> polygon, int x, int y)
+    {
+        var crossings = 0;
+        for (var i = 0; i < polygon.Count; i++)
+        {
+            var p1 = polygon[i];
+            var p2 = polygon[(i + 1) % polygon.Count];
+
+            if ((!(p1.Y <= y) || !(p2.Y > y)) && (!(p1.Y > y) || !(p2.Y <= y)))
+                continue;
+
+            var vt = (y - p1.Y) / (p2.Y - p1.Y);
+            if (x < p1.X + vt * (p2.X - p1.X))
+                crossings++;
+        }
+
+        return crossings % 2 == 1 && x >= 0 && x < MainPictureBox.Width && y >= 0 && y < MainPictureBox.Height;
+    }
+
+    private void SeedFillingButton_Click(object sender, EventArgs e)
+    {
+        if (_selectedPoint is null)
+        {
+            MessageBox.Show("Пожалуйста, выберите точку из которой желаете начать заливку", "Вниманиа!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            return;
+        }
+
+        if (!IsPointInPolygon(_originalPoints, _selectedPoint.Value.X, _selectedPoint.Value.Y))
+        {
+            MessageBox.Show("Выбранная точка не находится внутри фигуры", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+
+        var targetColor = _bitmap.GetPixel(_selectedPoint.Value.X, _selectedPoint.Value.Y);
+
+        var pixelsToFill = new Stack<Point>();
+        pixelsToFill.Push(new Point(_selectedPoint.Value.X, _selectedPoint.Value.Y));
+
+        while (pixelsToFill.Count > 0)
+        {
+            var currentPixel = pixelsToFill.Pop();
+            var x = currentPixel.X;
+            var y = currentPixel.Y;
+
+            if (x < 0 || x >= _bitmap.Width || y < 0 || y >= _bitmap.Height ||
+                _bitmap.GetPixel(x, y) != targetColor)
+                continue;
+
+            _bitmap.SetPixel(x, y, _fillColor);
+
+            MainPictureBox.Refresh();
+
+            pixelsToFill.Push(new Point(x + 1, y));
+            pixelsToFill.Push(new Point(x, y + 1));
+            pixelsToFill.Push(new Point(x - 1, y));
+            pixelsToFill.Push(new Point(x, y - 1));
+        }
+    }
+
+    private async void LineByLineFillingButton_Click(object sender, EventArgs e)
+    {
+        if (_selectedPoint is null)
+        {
+            MessageBox.Show("Пожалуйста, выберите точку из которой желаете начать заливку", "Вниманиа!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            return;
+        }
+
+        if (!IsPointInPolygon(_originalPoints, _selectedPoint.Value.X, _selectedPoint.Value.Y))
+        {
+            MessageBox.Show("Выбранная точка не находится внутри фигуры", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+
+        var currentX = _selectedPoint.Value.X;
+        var currentY = _selectedPoint.Value.Y;
+        var fillArray = new bool[_bitmap.Height, _bitmap.Width];
+
+
+        while (true)
+        {
+            fillArray[currentY, currentX] = true;
+            currentX += 1;
+           
+            if (currentX >= _bitmap.Width)
+            {
+                currentX = 0;
+                currentY += 1; 
+
+                if (currentY >= _bitmap.Height)
+                    break; 
+            }
+
+            while (currentX >= 0.0f && !IsPointInPolygon(_originalPoints, currentX, currentY))
+            {
+                currentX -= 1; // Изменение координаты X с плавающей точкой.
+            }
+
+            if (!IsPointInPolygon(_originalPoints, currentX, currentY)   )
+                break;
+        }
+
+        for (var y = 0; y < _bitmap.Height; y++)
+        {
+            for (var x = 0; x < _bitmap.Width; x++)
+            {
+                if (fillArray[y, x])
+                {
+                    _bitmap.SetPixel(x, y, _fillColor);
+                    await Task.Delay(1);
+                }
+            }
+        }
+    }
+}
