7133c80b778c19eed618e5e41642cc0dab332fcb
MurphyBLG <vitaliy.ryavkin@gmail.com>
2023-12-04 21:26:51 +0500

    фыва

---
+++ lab5/lab5/MainForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace lab5
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            MainPictureBox = new PictureBox();
+            NestingLevelLabel = new Label();
+            NestingLevelTextBox = new TextBox();
+            DrawButton = new Button();
+            ClearButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)MainPictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // MainPictureBox
+            // 
+            MainPictureBox.BorderStyle = BorderStyle.FixedSingle;
+            MainPictureBox.Location = new Point(12, 12);
+            MainPictureBox.Name = "MainPictureBox";
+            MainPictureBox.Size = new Size(1240, 536);
+            MainPictureBox.TabIndex = 0;
+            MainPictureBox.TabStop = false;
+            // 
+            // NestingLevelLabel
+            // 
+            NestingLevelLabel.AutoSize = true;
+            NestingLevelLabel.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            NestingLevelLabel.Location = new Point(33, 601);
+            NestingLevelLabel.Name = "NestingLevelLabel";
+            NestingLevelLabel.Size = new Size(275, 32);
+            NestingLevelLabel.TabIndex = 1;
+            NestingLevelLabel.Text = "Уровень вложенности: ";
+            // 
+            // NestingLevelTextBox
+            // 
+            NestingLevelTextBox.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            NestingLevelTextBox.Location = new Point(314, 594);
+            NestingLevelTextBox.Name = "NestingLevelTextBox";
+            NestingLevelTextBox.Size = new Size(145, 43);
+            NestingLevelTextBox.TabIndex = 2;
+            NestingLevelTextBox.Text = "1000";
+            // 
+            // DrawButton
+            // 
+            DrawButton.Location = new Point(578, 594);
+            DrawButton.Name = "DrawButton";
+            DrawButton.Size = new Size(281, 45);
+            DrawButton.TabIndex = 3;
+            DrawButton.Text = "Нарисовать";
+            DrawButton.UseVisualStyleBackColor = true;
+            DrawButton.Click += DrawButton_Click;
+            // 
+            // ClearButton
+            // 
+            ClearButton.Location = new Point(891, 594);
+            ClearButton.Name = "ClearButton";
+            ClearButton.Size = new Size(230, 45);
+            ClearButton.TabIndex = 4;
+            ClearButton.Text = "Очистить";
+            ClearButton.UseVisualStyleBackColor = true;
+            ClearButton.Click += ClearButton_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1264, 681);
+            Controls.Add(ClearButton);
+            Controls.Add(DrawButton);
+            Controls.Add(NestingLevelTextBox);
+            Controls.Add(NestingLevelLabel);
+            Controls.Add(MainPictureBox);
+            Name = "MainForm";
+            Text = "Лабораторная работа 5 Фракталы Рявкин Виталий Алексеевич";
+            ((System.ComponentModel.ISupportInitialize)MainPictureBox).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox MainPictureBox;
+        private Label NestingLevelLabel;
+        private TextBox NestingLevelTextBox;
+        private Button DrawButton;
+        private Button ClearButton;
+    }
+}
---
+++ lab5/lab5/MainForm.cs
@@ -0,0 +1,72 @@
+﻿using System.Windows.Forms;
+
+namespace lab5;
+public partial class MainForm : Form
+{
+    private int maxIterations = 1000; // Максимальное количество итераций
+    private double zoom = 250; // Масштаб
+    private readonly Bitmap _bitmap;
+
+    public MainForm()
+    {
+        InitializeComponent();
+        DoubleBuffered = true;
+        int width = MainPictureBox.Width;
+        int height = MainPictureBox.Height;
+
+        _bitmap = new Bitmap(width, height);
+    }
+
+    private void DrawFractal()
+    {
+        int width = MainPictureBox.Width;
+        int height = MainPictureBox.Height;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                double zx = (x - width / 2.0) / zoom;
+                double zy = (y - height / 2.0) / zoom;
+                double cx = zx;
+                double cy = zy;
+
+                int iteration = 0;
+                while (zx * zx + zy * zy < 4 && iteration < maxIterations)
+                {
+                    double tmp = zx * zx - zy * zy + cx;
+                    zy = 2.0 * zx * zy + cy;
+                    zx = tmp;
+                    iteration++;
+                }
+
+                // Преобразование числа итераций в оттенок серого
+                int shade = (int)(255.0 * iteration / maxIterations);
+
+                _bitmap.SetPixel(x, y, Color.FromArgb(shade, shade, shade));
+            }
+        }
+
+        MainPictureBox.Image = _bitmap;
+        MainPictureBox.Invalidate();
+    }
+
+    private void DrawButton_Click(object sender, EventArgs e)
+    {
+        maxIterations = Int32.Parse(NestingLevelTextBox.Text);
+        DrawFractal();
+    }
+
+    private void ClearImage()
+    {
+        var g = Graphics.FromImage(_bitmap);
+        g.Clear(Color.Empty);
+    }
+
+    private void ClearButton_Click(object sender, EventArgs e)
+    {
+        ClearImage();
+        MainPictureBox.Invalidate();
+
+    }
+}
